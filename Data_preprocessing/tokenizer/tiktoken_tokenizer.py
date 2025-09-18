a4268d945a228ca7f56cab371743f29fba08347b
Bethlehem <93065948+Betty987@users.noreply.github.com>
2025-09-18 14:09:59 +0300

    add the the tokenizer and dataloading

---
+++ Data_preprocessing/tokenizer/tiktoken_tokenizer.py
@@ -0,0 +1,55 @@
+import os
+import torch
+import tiktoken
+import numpy as np
+import requests
+from predictive_coding.config import GPTConfig
+from Data_preprocessing.config import Config
+
+input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
+if not os.path.exists(input_file_path):
+    data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
+    with open(input_file_path, 'w', encoding='utf-8') as f:
+        f.write(requests.get(data_url).text)
+
+with open(input_file_path, 'r', encoding='utf-8') as f:
+    data = f.read()
+n = len(data)
+train_data = data[:int(n*0.9)]
+val_data = data[int(n*0.9):]
+
+# encode with tiktoken gpt2 bpe
+enc = tiktoken.get_encoding("gpt2")
+train_ids = enc.encode_ordinary(train_data)
+val_ids = enc.encode_ordinary(val_data)
+print(f"train has {len(train_ids):,} tokens")
+print(f"val has {len(val_ids):,} tokens")
+# vocab_size = enc.n_vocab
+
+# export to bin files in 'data' directory
+data_dir = os.path.join(os.path.dirname(__file__), 'data')
+os.makedirs(data_dir, exist_ok=True)
+train_ids = np.array(train_ids, dtype=np.uint16)
+val_ids = np.array(val_ids, dtype=np.uint16)
+train_ids.tofile(os.path.join(data_dir, 'train.bin'))
+val_ids.tofile(os.path.join(data_dir, 'val.bin'))
+
+def get_batch(split):
+    # We recreate np.memmap every batch to avoid a memory leak, as per
+    # https://stackoverflow.com/questions/45132940/numpy-memmap-memory-usage-want-to-iterate-once/61472122#61472122
+    if split == 'train':
+        data = np.memmap(os.path.join(data_dir, 'train.bin'), dtype=np.uint16, mode='r')
+    else:
+        data = np.memmap(os.path.join(data_dir, 'val.bin'), dtype=np.uint16, mode='r')
+    ix = torch.randint(len(data) - GPTConfig.block_size, (GPTConfig.batch_size,))
+    x = torch.stack([torch.from_numpy((data[i:i+GPTConfig.block_size]).astype(np.int64)) for i in ix])
+    y = torch.stack([torch.from_numpy((data[i+1:i+1+GPTConfig.block_size]).astype(np.int64)) for i in ix])
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device_type = 'cuda' if 'cuda' in device else 'cpu'
+    if device_type == 'cuda':
+        # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
+        x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(device, non_blocking=True)
+    else:
+        x, y = x.to(device), y.to(device)
+    return x, y
+
